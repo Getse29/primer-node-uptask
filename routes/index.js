4f8343cba56d0e0fbfe12f0ab5358ab565fe4728
Josué Getsemani Sandoval Villarreal <getsemanilol29@gmail.com>
2021-05-15 09:56:01 -0700

    agregando controlador para rutas

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+/* importar controlador */
+const proyectosController = require('../controllers/proyectosController')
+
+
 module.exports = function () {
   //Runa para el home
-  router.get('/', (req, res) => {
-    res.send('Hola');
-  });
+  router.get('/', proyectosController.proyectosHome);
 
   //Ruta para Nosotros
-  router.get('/nosotros', (req, res) => {
-    res.send('Nosotros')
-  })
+  router.get('/nosotros', proyectosController.proyectoNosotros)
 
   return router
 }
